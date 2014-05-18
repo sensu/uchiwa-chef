c394842e4694d6127d4b26d7425fe0c51c5fafdc
Jean-Francois Theroux <me@failshell.io>
2014-05-17 22:54:30 -0400

    Boilerplates

---
+++ README.md
@@ -0,0 +1,13 @@
+# uchiwa cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Jean-Francois Theroux (<me@failshell.io>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'uchiwa'
+maintainer       'Jean-Francois Theroux'
+maintainer_email 'me@failshell.io'
+license          'Apache 2.0'
+description      'Installs/Configures uchiwa'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
