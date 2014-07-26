f09264850f0d033ca0253299b149e9bfc49b83cb
Jean-Francois Theroux <me@failshell.io>
2014-07-26 17:12:36 -0400

    added user/group

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,5 @@ default['uchiwa']['version'] = '0.1.5-2'
 default['uchiwa']['apt_repo_url'] = "http://repos.sensuapp.org/apt"
 default['uchiwa']['yum_repo_url'] = "http://repos.sensuapp.org"
 default['uchiwa']['use_unstable_repo'] = false
+default['uchiwa']['user'] = 'uchiwa'
+default['uchiwa']['group'] = 'uchiwa'
