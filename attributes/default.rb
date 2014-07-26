c9265ff3d7189cdc6936e216fa40288bccf0d8ec
Jean-Francois Theroux <me@failshell.io>
2014-07-26 16:48:56 -0400

    cleanup attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,5 @@
-default['uchiwa']['version'] = '0.1.3'
-default['uchiwa']['base_dir'] = '/opt/uchiwa'
-default['uchiwa']['log_dir'] = '/var/log/uchiwa'
-default['uchiwa']['user'] = 'uchiwa'
-default['uchiwa']['group'] = 'uchiwa'
-
-default['uchiwa']['api_server'] = 'localhost'
-default['uchiwa']['api_port'] = 4567
-
-# overrides
-override['nodejs']['version'] = '0.10.28'
-override['nodejs']['checksum_linux_x64'] = '5f41f4a90861bddaea92addc5dfba5357de40962031c2281b1683277a0f75932'
+# Global
+default['uchiwa']['version'] = '0.1.5-2'
+default['uchiwa']['apt_repo_url'] = "http://repos.sensuapp.org/apt"
+default['uchiwa']['yum_repo_url'] = "http://repos.sensuapp.org"
+default['uchiwa']['use_unstable_repo'] = false
