fdff139cd81877b4c7d3dd279e24de95c9762e2e
Jean-Francois Theroux <me@failshell.io>
2014-07-29 12:32:59 -0400

    updated to uchiwa 0.1.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # Global
-default['uchiwa']['version'] = '0.1.5-2'
+default['uchiwa']['version'] = '0.1.6-1'
 default['uchiwa']['apt_repo_url'] = "http://repos.sensuapp.org/apt"
 default['uchiwa']['yum_repo_url'] = "http://repos.sensuapp.org"
 default['uchiwa']['use_unstable_repo'] = false
