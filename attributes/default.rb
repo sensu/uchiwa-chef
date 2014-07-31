3f11ffb2d10c87e74293c571794180a53fe016b2
Jean-Francois Theroux <me@failshell.io>
2014-07-31 15:38:44 -0400

    updated attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,23 @@
 # Global
-default['uchiwa']['version'] = '0.1.6-1'
+default['uchiwa']['version'] = '0.1.7-1'
 default['uchiwa']['apt_repo_url'] = "http://repos.sensuapp.org/apt"
 default['uchiwa']['yum_repo_url'] = "http://repos.sensuapp.org"
 default['uchiwa']['use_unstable_repo'] = false
-default['uchiwa']['user'] = 'uchiwa'
+default['uchiwa']['owner'] = 'uchiwa'
 default['uchiwa']['group'] = 'uchiwa'
+
+# Uchiwa Settings
+default['uchiwa']['settings']['user'] = 'admin'
+default['uchiwa']['settings']['pass'] = 'supersecret'
+default['uchiwa']['settings']['stats'] = 10
+default['uchiwa']['settings']['refresh'] = 10000
+default['uchiwa']['settings']['bind_address'] = '0.0.0.0'
+default['uchiwa']['settings']['bind_port'] = 3000
+
+# APIs Settings
+default['uchiwa']['api'] = {
+  'name' => 'Sensu',
+  'host' => '127.0.0.1',
+  'ssl' => false,
+  'timeout' => 5000
+}
