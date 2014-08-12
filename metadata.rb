93bac230da475ff3d2ec371e8073d76b53004d36
Jean-Francois Theroux <me@failshell.io>
2014-08-12 16:07:38 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'me@failshell.io'
 license          'Apache 2.0'
 description      'Installs/Configures uchiwa'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.3'
+version          '0.3.4'
 
 depends          'yum'
 depends          'apt'
