9be05148b6f51cb8b7ef810ce461518220f84287
Justin Kolberg <amd.prophet@gmail.com>
2014-12-06 17:36:13 -0800

    Make foodcritic happy

--- recipes/http.rb
+++ recipes/http.rb
@@ -17,17 +17,16 @@
 # limitations under the License.
 
 platform_family = node['platform_family']
-platform_version = node['platform_version'].to_i
 
 case platform_family
 when 'debian'
   package_options = node['uchiwa']['package_options'] || '--force-confdef --force-confnew'
 
   arch = if node['kernel']['machine'] == 'x86_64'
-    'amd64'
-  else
-    'i386'
-  end
+           'amd64'
+         else
+           'i386'
+         end
 
   pkg = "uchiwa_#{node['uchiwa']['version']}_#{arch}.deb"
   url = "#{node['uchiwa']['http_url']}/#{pkg}"
@@ -44,10 +43,10 @@ when 'rhel'
   package_options = node['uchiwa']['package_options'] || '--nogpgcheck'
 
   arch = if node['kernel']['machine'] == 'i686'
-    'i386'
-  else
-    "#{node['kernel']['machine']}"
-  end
+           'i386'
+         else
+           node['kernel']['machine']
+         end
 
   pkg = "uchiwa-#{node['uchiwa']['version']}.#{arch}.rpm"
   url = "#{node['uchiwa']['http_url']}/#{pkg}"
