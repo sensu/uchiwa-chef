a87d437247fd52bea296dc55bd15233d6ec80e4c
Justin Kolberg <amd.prophet@gmail.com>
2014-12-08 17:05:20 -0800

    Release 0.7.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+uchiwa-chef Cookbook CHANGELOG
+==============================
+This file is used to list changes made in each version of the uchiwa-chef
+cookbook.
+
+v0.7.0 (2014-12-08)
+-------------------
+- Set default package options (apt/dpkg/yum)
+- Allow default package options to be overridden
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'uchiwa'
-maintainer       'Jean-Francois Theroux'
-maintainer_email 'me@failshell.io'
+maintainer       'Justin Kolberg'
+maintainer_email 'amd.prophet@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures uchiwa'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.0'
+version          '0.7.0'
 
 depends          'yum'
 depends          'apt'
