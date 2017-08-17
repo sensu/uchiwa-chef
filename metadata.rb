4c7edcb25527aa1e27730400190b71a36c08a1ef
Tim Smith <tsmith@chef.io>
2017-08-17 14:02:59 -0700

    Require Chef 12.1+ and update metadata
    Chef 11 is officially EOL now
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'uchiwa'
 maintainer       'Justin Kolberg'
 maintainer_email 'amd.prophet@gmail.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures uchiwa'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.3.0'
@@ -19,3 +19,7 @@ depends          'apt'
 ).each do |os|
   supports os
 end
+
+source_url 'https://github.com/sensu/uchiwa-chef'
+issues_url 'https://github.com/sensu/uchiwa-chef/issues'
+chef_version '>= 12.1' if respond_to?(:chef_version)
